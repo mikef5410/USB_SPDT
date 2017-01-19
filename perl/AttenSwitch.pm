ff556a13f0d9fb2ef1de9deeb7b117f46de76970
Mike Ferrara <mikef@broadcom.com>
2017-01-19 11:44:30 -0800

    Add self-doc to AttenSwitch.pm, tidy sources

--- perl/AttenSwitch.pm
+++ perl/AttenSwitch.pm
@@ -1,3 +1,14 @@
+# ******************************************************************************
+# Copyright (C) 2015 Michael R. Ferrara, All rights reserved.
+#
+# Santa Rosa, CA 95404
+# Tel:(707)536-1330
+#
+
+# Control USB Attenuator/Switch/Stacklight microcontroller
+# via packet protocol over bulk channel
+#
+
 package AttenSwitch;
 use Device::USB;
 use Moose;
@@ -7,6 +18,135 @@ use namespace::autoclean;
 ## no critic (BitwiseOperators)
 ## no critic (ValuesAndExpressions::ProhibitAccessOfPrivateData)
 #
+
+=head1 NAME
+
+AttenSwitch - Communicate with USB Attenuator/Switch/Stacklight driver via packet protocol over bulk channel
+
+=head1 VERSION
+
+VERSION 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+=over 4
+
+  use strict;
+  use warnings;
+  use feature qw(switch);
+  no warnings 'experimental';
+
+  #Where to find our library and auxiliary code.
+  use FindBin qw($Bin);
+  use lib $ENV{ATTENSW} || "/projects/usbAtten_controller/perl";
+
+  use lib "$Bin/..";
+  use lib "$Bin";
+
+  my $att = shift(@ARGV);
+
+  use AttenSwitch;
+
+  my $dut    = AttenSwitch->new(PID => 0x0003, SERIAL => "138001F0" );
+  my $result = $dut->connect();
+  if ( $result == AttenSwitch::SUCCESS ) {
+     print("Got device\n");
+  }
+
+  my $attSel;
+
+  given ($att) {
+    when (0)  { $attSel = AttenSwitch::ATTEN->ATT_0DB; }
+    when (10) { $attSel = AttenSwitch::ATTEN->ATT_10DB; }
+    when (20) { $attSel = AttenSwitch::ATTEN->ATT_20DB; }
+    when (30) { $attSel = AttenSwitch::ATTEN->ATT_30DB; }
+    when (40) { $attSel = AttenSwitch::ATTEN->ATT_40DB; }
+    when (50) { $attSel = AttenSwitch::ATTEN->ATT_50DB; }
+    when (60) { $attSel = AttenSwitch::ATTEN->ATT_60DB; }
+    when (70) { $attSel = AttenSwitch::ATTEN->ATT_70DB; }
+  }
+  $dut->atten($attSel);
+  printf("ok\n");
+  exit;
+
+=back
+
+=head1 DESCRIPTION
+
+This module actually implements three different devices, a Stacklight driver, a step-attenuator driver, and a combo SP8T + SPDT driver.
+The drivers are custom designed STM32F411 usb devices that identify with VID:0x4161, and PID:0x0001 for stacklight, 0x0002 for SP8T+SPDT,
+and 0x0003 for attenuator. The devices also implement a CDC-ACM device and have a debug monitor running on that interface. The protocol 
+used by this module is a simple COMMAND-RESPONSE packet protocol implemented on bulk endpoints 0x1/0x81.
+
+
+=head1 Class Atributes
+
+=over 4
+
+=item *
+
+B<validVids> - An array ref of Vids this module will recognize. Defaults to [ 0x4161, ]
+
+=item *
+
+B<validPids> - An array ref of Pids this module will recognize, Defaults to  [ 0x00ff, 0x0003, 0x0001, 0x0002 ]
+
+=back
+
+=head1 Object Attributes
+
+=over 4
+
+=item *
+
+B<dev> - The underlying USB device (Device::USB::Device)
+
+=item *
+
+B<usb> - The underlying USB bus (Device::USB)
+
+=item *
+
+B<VID> - A specific VID to which we want to connect
+
+=item *
+
+B<PID> - A specific PID to which we want to connect
+
+=item *
+
+B<SERIAL> - A specific USB serial number to which we want to connect
+
+=item *
+
+B<PRODINFO> - When connected, the device identity is queried, and this object reflects that info. It is
+a AttenSwitch::ProdInfo object.
+
+=item *
+
+B<verbose> - Boolean, be talkative
+
+=item *
+
+B<timeout_ms> - Number, milliseconds of timeout. Defaults to 500ms
+
+=item *
+
+B<manufacturer> - Manufacturer string. My stuff returns "MF"
+
+=item *
+
+B<product> - Product string.
+
+
+=back
+
+=cut
+
 # Class Attributes
 class_has 'validVids' => (
   is      => 'ro',
@@ -83,6 +223,19 @@ has 'product' => (
   default => '',
 );
 
+=head2 METHODS
+
+=over 4
+
+=item B<< $attenswitch->connect() >> 
+
+Find the usb device and get a connection to it. By specifying VID,PID,and SERIAL at construction (or some combination) you should
+be able to talk to a specific device.
+
+=back
+
+=cut
+
 sub connect {
   my $self = shift;
 
@@ -91,7 +244,7 @@ sub connect {
   my $vid;
   my $pid;
   my $dev;
-  
+
   foreach $vid (@vids) {
     foreach $pid (@pids) {
       if ( $self->has_SERIAL ) {
@@ -112,7 +265,7 @@ sub connect {
     print "ERROR: could not find any AttenSwitch devices \n";
     return AttenSwitch->FAIL;
   }
-  
+
 FOUND:
   $self->VID( $dev->idVendor() );
   $self->PID( $dev->idProduct() );
@@ -141,7 +294,7 @@ FOUND:
     }
     printf("\n") if ( $self->verbose() );
   }
-  my $claim = $dev->claim_interface(0x2); #Interface #2 is my command I/O interface
+  my $claim = $dev->claim_interface(0x2);    #Interface #2 is my command I/O interface
   printf("Claim returns  $claim \n") if ( $self->verbose() );
   $self->dev($dev);
 
@@ -151,15 +304,39 @@ FOUND:
   return AttenSwitch->SUCCESS;
 }
 
+=over 4
+
+=item B<< $attenswitch->disconnect() >> 
+
+Notify USB subsystem you're no longer interested in this device interface.
+You can do this when done with the device.
+
+=back
+
+=cut
+
 sub disconnect {
   my $self = shift;
   $self->dev->release_interface(0x2);
   undef( $self->{dev} );
 }
 
+=over 4
+
+=item B<< ($err,$rxPacket) = $attenswitch->send_packet($txPacket) >> 
+
+Low level send a packet to the device. $rxPacket and $txPacket are AttenSwitch::Packet objects.
+
+$err will be either AttenSwitch->SUCCESS or AttenSwitch->FAIL.
+
+=back
+
+=cut
+
 sub send_packet {
-  my $self     = shift;
-  my $packet   = shift;                        #AttenSwitch::Packet;
+  my $self   = shift;
+  my $packet = shift;    #AttenSwitch::Packet;
+
   my $rxPacket = AttenSwitch::Packet->new();
   if ( defined( $self->dev ) ) {
     if ( ref($packet) && $packet->isa("AttenSwitch::Packet") ) {
@@ -188,9 +365,23 @@ sub send_packet {
   return ( AttenSwitch->FAIL, $rxPacket );
 }
 
+=over 4
+
+=item B<< $err = $attenswitch->sp8t($select) >> 
+
+Set the SP8T switch. $select is an AttenSwitch::SP8TSETTING, a Class::Enum of: qw(J1 J2 J3 J4 J5 J6 J7 J8)
+
+$err will be either AttenSwitch->SUCCESS or AttenSwitch->FAIL.
+
+=back
+
+
+=cut
+
 sub sp8t {
-  my $self   = shift;
-  my $sel    = shift;                      #AttenSwitch::SP8TSETTING
+  my $self = shift;
+  my $sel  = shift;    #AttenSwitch::SP8TSETTING
+
   my $outPkt = AttenSwitch::Packet->new(
     command => AttenSwitch::COMMAND->SP8T,
     payload => pack( "C", $sel->ordinal )
@@ -199,10 +390,23 @@ sub sp8t {
   return ($res);
 }
 
+=over 4
+
+=item B<< $err = $attenswitch->spdt($switch, $select) >> 
+ 
+Set one of the two available SPDTs. $switch is a AttenSwitch::SPDTSEL, a Class::Enum of: qw(SW1 SW2) and $select is a AttenSwitch::SPDTSETTING, a Class::Enum of qw(J1SEL J2SEL).
+
+$err will be either AttenSwitch->SUCCESS or AttenSwitch->FAIL.
+
+=back
+
+=cut
+
 sub spdt {
-  my $self   = shift;
-  my $sw     = shift;                      #AttenSwitch::SPDTSEL
-  my $set    = shift;                      #AttenSwitch::SPDTSETTING
+  my $self = shift;
+  my $sw   = shift;    #AttenSwitch::SPDTSEL
+  my $set  = shift;    #AttenSwitch::SPDTSETTING
+
   my $outPkt = AttenSwitch::Packet->new(
     command => AttenSwitch::COMMAND->SPDT,
     payload => pack( "CC", $sw->ordinal, $set->ordinal )
@@ -211,9 +415,23 @@ sub spdt {
   return ($res);
 }
 
+=over 4
+
+=item B<< $err = $attenswitch->atten($attenuation) >> 
+
+Set a 0-70dB step attenuator. $attenuation is a AttenSwitch::ATTEN, a Class::Enum of: qw(ATT_0DB ATT_10DB ATT_20DB ATT_30DB ATT_40DB
+ATT_50DB ATT_60DB ATT_70DB)
+
+$err will be either AttenSwitch->SUCCESS or AttenSwitch->FAIL.
+
+=back
+
+=cut
+
 sub atten {
-  my $self   = shift;
-  my $sel    = shift;                      #AttenSwitch::ATTEN
+  my $self = shift;
+  my $sel  = shift;    #AttenSwitch::ATTEN
+
   my $outPkt = AttenSwitch::Packet->new(
     command => AttenSwitch::COMMAND->ATT,
     payload => pack( "C", $sel->ordinal )
@@ -222,13 +440,26 @@ sub atten {
   return ($res);
 }
 
+=over 4
+
+=item B<< $err = $attenswitch->stacklightnotify($color,$onTime,$offTime,$count) >> 
+
+Command the stacklight to turn on a light. $color is a string: "r,y, or g", $onTime and $offTime are in milliseconds, and $count is the number of times to blink. A count of 0 is blink indefinitely. To extinguish, send a new notify with a short blink, and count of 1. Solid on can be emulated with a long on time and very short off time.
+
+$err will be either AttenSwitch->SUCCESS or AttenSwitch->FAIL.
+
+=back
+
+=cut
+
 sub stacklightNotify {
   my $self    = shift;
   my $color   = uc(shift);
   my $onTime  = shift || 0;
   my $offTime = shift || 0;
   my $count   = shift || 0;
-  my $col     = 0;
+
+  my $col = 0;
   $col |= 0x1 if ( $color =~ /R/ );
   $col |= 0x2 if ( $color =~ /Y/ );
   $col |= 0x4 if ( $color =~ /G/ );
@@ -240,12 +471,24 @@ sub stacklightNotify {
   return ($res);
 }
 
+=over 4
+
+=item B<< $return = $attenswitch->readEE($addr,$nbytes) >> 
+
+Returns a string of bytes.
+
+=back
+
+
+=cut
+
 sub readEE {
   my $self   = shift;
   my $addr   = shift;
   my $nbytes = shift;
-  my $ret    = "";
-  my $k      = 0;
+
+  my $ret = "";
+  my $k   = 0;
   my $outPkt;
   for ( $k = 0 ; $k < $nbytes ; $k++ ) {
     $outPkt = AttenSwitch::Packet->new(
@@ -259,11 +502,23 @@ sub readEE {
   return ($ret);
 }
 
+=over 4
+
+=item B<< $attenswitch->writeEE($addr,$val) >> 
+
+Writes $val to $addr in EEprom ... $val can be a string of more than one byte.
+Be careful, the EEprom is where the device stores its identifying info.
+
+=back
+
+=cut
+
 sub writeEE {
   my $self = shift;
   my $addr = shift;
   my $val  = shift;    # string of bytes
-  my $k    = 0;
+
+  my $k = 0;
   my $outPkt;
   for ( $k = 0 ; $k < length($val) ; $k++ ) {
     $outPkt = AttenSwitch::Packet->new(
@@ -274,8 +529,19 @@ sub writeEE {
   }
 }
 
+=over 4
+
+=item B<< $attenswitch->eraseAllEE() >> 
+
+Bulk erase the EEprom. Dangerous.
+
+=back
+
+=cut
+
 sub eraseAllEE {
-  my $self   = shift;
+  my $self = shift;
+
   my $outPkt = AttenSwitch::Packet->new(
     command => AttenSwitch::COMMAND->ERASEALL,
     payload => ""
@@ -283,6 +549,16 @@ sub eraseAllEE {
   my ( $res, $rxPacket ) = $self->send_packet($outPkt);
   return ($res);
 }
+
+=over 4
+
+=item B<< $info = $attenswitch->identify() >> 
+
+Ask the device to self-identify. Returns an AttenSwitch::ProdInfo object.
+
+=back
+
+=cut
 
 sub identify {
   my $self = shift;
