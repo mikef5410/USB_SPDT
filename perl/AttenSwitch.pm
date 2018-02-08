e12e30ae5196b0fef08ac7a152a874da3edf03b5
Mike Ferrara <mikef@broadcom.com>
2018-02-08 14:41:22 -0800

    Add enumeration for USB_DUALSPDT PID

--- perl/AttenSwitch.pm
+++ perl/AttenSwitch.pm
@@ -816,5 +816,6 @@ use Class::Enum (
   PROD_STACKLIGHT => { ordinal => 1 },
   PROD_MAPLEOLT   => { ordinal => 2 },
   PROD_ATTEN70    => { ordinal => 3 },
+  PROD_DUALSPDT   => { ordinal => 4 },
 );
 1;
